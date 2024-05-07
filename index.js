ee44bb8a8533e27bb844d70c4522d66bbd432e2a
Adnanh4141 <adnanh4141@gmail.com>
2024-05-07 17:52:23 +0600

    34

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const app = require("./app");
+// const app = require("./app");
+const app = express()
 const connectDB = require("./config/db");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const port = process.env.PORT || 3000;
+
+app.get('/' , (req , res)=>{
+  res.send('hello from simple server')
+})
+app.get('/products' , (req , res)=>{
+  res.send('hello from Products page')
+})
 
 // const dbURL = `mongodb+srv://Adnan:LPL1MheB0B4bsCvg@cluster0.mpobb6h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // connectDB(dbURL)
