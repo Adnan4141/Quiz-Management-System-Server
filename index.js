af0a3697bbe7b7365517e5e3dcc27480777f818f
Adnanh4141 <adnanh4141@gmail.com>
2024-05-07 18:05:28 +0600

    mongodb server on

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const express = require('express')
 // const app = require("./app");
 const app = express()
 const connectDB = require("./config/db");
-// const bcrypt = require("bcrypt");
-// const jwt = require("jsonwebtoken");
+
 require("dotenv").config();
 const port = process.env.PORT || 3000;
 
@@ -15,16 +14,16 @@ app.get('/products' , (req , res)=>{
   res.send('hello from Products page')
 })
 
-// const dbURL = `mongodb+srv://Adnan:LPL1MheB0B4bsCvg@cluster0.mpobb6h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-// connectDB(dbURL)
-// .then(() => {
-//   console.log(`Connected to MongoDB at ${dbURL}`);
-//   console.log(`Server is up and running on http://localhost:${port}`);
-// })
-// .catch((error) => {
-//   console.error("Failed to connect to MongoDB:", error);
-//   process.exit(1); 
-// });
+const dbURL = `mongodb+srv://Adnan:LPL1MheB0B4bsCvg@cluster0.mpobb6h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+connectDB(dbURL)
+.then(() => {
+  console.log(`Connected to MongoDB at ${dbURL}`);
+  console.log(`Server is up and running on http://localhost:${port}`);
+})
+.catch((error) => {
+  console.error("Failed to connect to MongoDB:", error);
+  process.exit(1); 
+});
 
 
 
