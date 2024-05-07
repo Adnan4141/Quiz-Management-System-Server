74f596959944a63a9b965e1ad7fda4bef61cba21
Adnanh4141 <adnanh4141@gmail.com>
2024-05-07 18:00:55 +0600

    connect db comment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express')
 // const app = require("./app");
 const app = express()
-const connectDB = require("./config/db");
+// const connectDB = require("./config/db");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
