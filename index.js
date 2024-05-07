f40566467a96ddb42f7966fc7d6564a85cf7eeb0
Adnanh4141 <adnanh4141@gmail.com>
2024-05-07 17:50:45 +0600

    3

--- index.js
+++ index.js
@@ -3,23 +3,22 @@ const connectDB = require("./config/db");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-
-
-const dbURL = `mongodb+srv://Adnan:LPL1MheB0B4bsCvg@cluster0.mpobb6h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-connectDB(dbURL)
-.then(() => {
-  console.log(`Connected to MongoDB at ${dbURL}`);
-  console.log(`Server is up and running on http://localhost:${port}`);
-})
-.catch((error) => {
-  console.error("Failed to connect to MongoDB:", error);
-  process.exit(1); 
-});
-
-
-
 const port = process.env.PORT || 3000;
+
+// const dbURL = `mongodb+srv://Adnan:LPL1MheB0B4bsCvg@cluster0.mpobb6h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// connectDB(dbURL)
+// .then(() => {
+//   console.log(`Connected to MongoDB at ${dbURL}`);
+//   console.log(`Server is up and running on http://localhost:${port}`);
+// })
+// .catch((error) => {
+//   console.error("Failed to connect to MongoDB:", error);
+//   process.exit(1); 
+// });
+
+
+
+
 
  app.listen( port, ()=> {
   //  const dbURL = process.env.DB_URL|| "mongodb://localhost:27017/QuizManagement";
